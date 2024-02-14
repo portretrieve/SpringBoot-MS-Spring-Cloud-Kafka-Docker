61d9157dcf6d8330fef25b121c01ef86ef00aa05
Devesh Kumar <portretrieve@yahoo.com>
2024-02-14 14:54:39 -0700

    Updated Entity Validation

--- inventory-service/src/main/java/com/programmingdevesh/inventoryservice/entity/InventoryItem.java
+++ inventory-service/src/main/java/com/programmingdevesh/inventoryservice/entity/InventoryItem.java
@@ -1,6 +1,8 @@
 package com.programmingdevesh.inventoryservice.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,10 @@ public class InventoryItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Must provide a name for item")
     private String itemName;
+
+    @Min(value = 1, message = "Must provide minimum quantity 1")
     private Integer quantity;
 }
